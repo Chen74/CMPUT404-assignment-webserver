861e7a317b8d6353e141b74235c0d93e2bee8a7c
Chen74 <54929297+Chen74@users.noreply.github.com>
2022-01-25 17:11:46 -0700

    first draft

--- server.py
+++ server.py
@@ -1,5 +1,9 @@
-#  coding: utf-8 
+#  coding: utf-8
+import mimetypes
 import socketserver
+import os
+from datetime import date
+
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
 # 
@@ -28,11 +32,78 @@ import socketserver
 
 
 class MyWebServer(socketserver.BaseRequestHandler):
-    
+
     def handle(self):
         self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+        print("Got a request of: %s\n" % self.data)
+        self.request.sendall(bytearray("OK", 'utf-8'))
+        header = self.data.decode().split("\n")
+        info = header[0].split()
+        # print(header.split('\n'))
+        file_path = info[1]
+        print(file_path)
+        count = 0
+        if info[0].upper() == "GET":
+            if file_path == "/":
+                if file_path.endswith("/") or file_path.endswith(""):
+                    file_path = "./www" + file_path + "index.html"
+                    if ".html" in file_path or ".css" in file_path:
+                        # mimetype = mimetypes.guess_type(file_path)
+                        self.successful_200(file_path)
+                        print(file_path)
+                    else:
+                        self.not_found_404()
+                else:
+                    self.not_found_404()
+            else:
+                if not (file_path[-1] == "/" and "." in file_path):
+                    # print(file_path)
+                    self.redirect_301(file_path)
+                elif file_path.endswith("/") and "." in file_path:
+                    self.not_found_404()
+
+            # path = os.path.realpath(os.getcwd() + "/www" + file_path)
+
+            if ".." in "./www" + file_path:
+                count = count - 1
+                if count < 0:
+                    self.not_found_404()
+
+        else:
+            self.not_allowed_405()
+
+    def not_found_404(self):
+        info = "HTTP/1.1 404 Not Found\r\n"
+        self.request.sendall(bytearray(info, 'utf-8'))
+
+    def not_allowed_405(self):
+        info = "HTTP/1.1 405 Method Not Allowed\r\n"
+        self.request.sendall(bytearray(info, 'utf-8'))
+
+    def successful_200(self, path):
+        info = "HTTP/1.1 200 OK\r\n"
+        ftype = str(self.type_check(path))
+        content_type = "Content-Type: " + ftype + "; charset=UTF-8\r\n"
+        response = info + content_type
+        self.request.sendall(bytearray(response + open(path).read(), 'utf-8'))
+
+    def redirect_301(self, path):
+        info = '301 Moved Permanently\r\n'
+        location = "Location: %s\r\n".format(path)
+        ftype = str(self.type_check(path))
+        content_type = "Content-Type: " + ftype + "; charset=UTF-8\r\n"
+        Date = "Date: %s\r\n".format(date.today().strftime("%d/%m/%Y"))
+        response = info + location + content_type + Date
+        self.request.sendall(bytearray(response + open(path).read(), 'utf-8'))
+
+    def type_check(self, path):
+        if ".css" in path:
+            file_type = "text/css"
+            return file_type
+        elif ".html" in path:
+            file_type = "text/html"
+            return file_type
+
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
@@ -44,3 +115,7 @@ if __name__ == "__main__":
     # Activate the server; this will keep running until you
     # interrupt the program with Ctrl-C
     server.serve_forever()
+
+# https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
+# https://docs.python.org/2/library/os.path.html
+# https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types
